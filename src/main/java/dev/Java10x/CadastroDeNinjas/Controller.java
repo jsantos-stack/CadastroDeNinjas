8d6179190fae9a1dbb4f7ebc7c3494e011ae9de2
João Davi <jdpds4016@gmail.com>
2026-07-28 17:34:49 -0300

    Criado a controle (Aula 2)

---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,19 @@
+package dev.Java10x.CadastroDeNinjas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // isso é um controlador
+@RequestMapping // para colocar todas as rotas no mesmo lugar
+public class Controller {
+    @GetMapping("/boasvindas") //criando a rota
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+
+
+
+
+
+}
